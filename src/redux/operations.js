da89b879b348b13b3f362f9d335823b4021f8e4d
wackytsui <allan.altavano@gmail.com>
2024-09-26 21:10:53 +0800

    api

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,25 +1,6 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// axios.defaults.baseURL = 'https://661fbcb516358961cd955014.mockapi.io/api/';
-
-// export const fetchContacts = () => async dispatch => {
-//   try {
-//     // Load indicator
-//     dispatch(fetchingInProgress());
-//     // HTTP request
-//     const response = await axios.get('/contacts');
-//     // Data processing
-//     dispatch(fetchingSuccess(response.data));
-//   } catch (e) {
-//     // Error processing
-//     dispatch(fetchingError(e.message));
-//   }
-// };
-
-/////////////////================= SYNTAX =======================///////////
-// createAsyncThunk('ACTION TYPE', async FUNCTION (args, thunkAPI) => {});
-
 // Async thunk for fetching contacts
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchContacts',
