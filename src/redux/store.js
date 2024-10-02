fab475ee8032b04908ecac621bebfcb9aa7740f8
Mark Allan Altavano <143721243+wackytsui@users.noreply.github.com>
2024-10-02 11:45:48 +0800

    operation

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,6 @@
 // store.js
 
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsSlice } from './contactsSlice';
